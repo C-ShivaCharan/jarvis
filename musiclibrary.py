499015da7d8c2880bc6dd570c2d579b3893dbe14
Shivacharan <shivacharanofficial2004@gmail.com>
2025-04-21 09:54:58 +0530

    voice assistant

---
+++ musiclibrary.py
@@ -0,0 +1,5 @@
+music={
+    "perfect":"https://www.youtube.com/watch?v=kPhpHvnnn0Q",
+    "gangster":"https://www.youtube.com/watch?v=V79D8__4W9g",
+    "7years":"https://www.youtube.com/watch?v=kqFLQG484pg"
+}
